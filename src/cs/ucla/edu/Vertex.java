88061f6fd2fd20303cffdf4f06929c28d118fb11
victor <victor@victor-PowerEdge-T110-II>
2012-08-15 18:22:34 -0700

    p

---
+++ src/cs/ucla/edu/Vertex.java
@@ -0,0 +1,20 @@
+package cs.ucla.edu;
+
+import java.util.Iterator;
+
+public class Vertex {
+
+	public void compute(Iterator<Message> msgs) {
+		
+	}
+	
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
